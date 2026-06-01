9c242cf75dbffb08038df0cb3db30d34d06a7a61
Muhammad Malik Fahd <mmalikfahd@gmail.com>
2026-06-01 09:56:48 +0700

    Upgrade Next.js to 15.2.6 and add ESLint Next.js plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
 import js from "@eslint/js"
 import globals from "globals"
 import tseslint from "typescript-eslint"
+import nextPlugin from "@next/eslint-plugin-next"
 
 export default tseslint.config(
   { ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: nextPlugin.configs.recommended.rules,
+  },
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
